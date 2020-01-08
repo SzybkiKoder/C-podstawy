817ed2fe85487b9f4052d5ac02992d81873deda9
Adrian <adrian.bernecki@wp.pl>
2020-01-09 00:12:07 +0100

    Laborki 1 zadanie

---
+++ LAB1WSEI.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+/*    string imie, nazwa_ulicy, miasto, nr_domu, kod_pocztowy;
+
+    cout << "Podaj imie i nazwisko" << endl;
+    getline(cin, imie);
+    cout << "Podaj nazwe ulicy" << endl;
+    cin >> nazwa_ulicy;
+    cout << "Podaj numer domu/mieszkania" << endl;
+    cin >> nr_domu;
+    cout << "Podaj kod pocztowy" << endl;
+    cin >> kod_pocztowy;
+    cout << "Podaj miasto" << endl;
+    cin >> miasto;
+
+    cout << "Twoje imie i nazwisko to: " << imie << endl;
+    cout << "Twoja nazwa ulicy to: " << nazwa_ulicy << ", a twoj numer domu/mieszkanie to: " << nr_domu << endl;
+    cout << "Twoj kod pocztowy to:  " << kod_pocztowy << ", a twoje miasto to: " << miasto << endl;
+
+    ZADANIE 2 LAB 1
+
+    int rok_urodzenia, aktualny_rok = 2020, wiek;
+
+    cout << "Podaj rok urodzenia" << endl;
+    cin >> rok_urodzenia;
+
+    wiek = aktualny_rok - rok_urodzenia;
+
+    cout << "Twoj wiek to: " << wiek << endl;*/
+
+
+
+
+
+    return 0;
+}
